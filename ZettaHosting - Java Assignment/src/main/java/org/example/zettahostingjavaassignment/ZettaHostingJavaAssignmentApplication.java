d1d6a149e945b9745b3755a79608ad1e707d6427
dinkovachev <147801675+dinkovachev@users.noreply.github.com>
2024-04-01 13:42:10 +0300

    SpringBoot Project init commit

---
+++ ZettaHosting - Java Assignment/src/main/java/org/example/zettahostingjavaassignment/ZettaHostingJavaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.zettahostingjavaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettaHostingJavaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZettaHostingJavaAssignmentApplication.class, args);
+    }
+
+}
---
+++ ZettaHosting - Java Assignment/src/test/java/org/example/zettahostingjavaassignment/ZettaHostingJavaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zettahostingjavaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettaHostingJavaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
